67751d6940a913d252dbcfcb7eb7f5e9d2c9dfa8
Serhii Manko <msa01051984@gmail.com>
2023-08-17 16:26:28 +0300

    url update

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -10,7 +10,7 @@ function Form() {
   const handleSubmit = async (values, { setSubmitting, resetForm }) => {
     try {
       const response = await axios.post(
-        "http://localhost:5000/register",
+        "https://zohocrmserver.onrender.com/register",
         values,
         {
           headers: {
